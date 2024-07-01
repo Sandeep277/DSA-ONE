721492899c93d9b0704943df69ba98adbec1cdfa
Sandeep Kushwaha <102976365+Sandeep277@users.noreply.github.com>
2024-07-01 14:32:31 +0530

    added binary tree

---
+++ src/linkList/InterviewQuestions.java
@@ -0,0 +1,35 @@
+package linkList;
+
+public class InterviewQuestions {
+
+    // https://leetcode.com/problems/linked-list-cycle
+    // Amazon and Microsoft
+    // Important Concept : fast pinter and slow pointer
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {
+    }
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ src/trees/BinaryTree.java
@@ -0,0 +1,94 @@
+package trees;
+
+import java.util.Scanner;
+
+public class BinaryTree {
+    Node root;
+
+    public class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node (int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // insert element
+    public void populate(Scanner scanner) {
+        System.out.println("Enter the root Node: ");
+        int value = scanner.nextInt();
+        root = new Node(value);
+        populate(scanner, root);
+    }
+    public void populate(Scanner scanner, Node node) {
+        System.out.println("Do you want to enter left of " + node.value);
+        boolean left = scanner.nextBoolean();
+        if (left) {
+            System.out.println("Enter the value of the left of " + node.value);
+            int value = scanner.nextInt();
+            node.left = new Node(value);
+            populate(scanner, node.left);
+        }
+
+        System.out.println("Do you want to enter left of " + node.value);
+        boolean right = scanner.nextBoolean();
+        if (right) {
+            System.out.println("Enter the value of the right of " + node.value);
+            int value = scanner.nextInt();
+            node.right = new Node(value);
+            populate(scanner, node.right);
+        }
+    }
+
+    // In order ( Left - Node - Right )
+    public void inOrder (Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.value + " ");
+        inOrder(node.right);
+    }
+
+    // Pre order ( Node - Left - Right )
+    public void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    // Post order ( Left - Right - Node )
+    public void postOrder (Node node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value + " ");
+    }
+
+    // display
+    public void display() {
+        display(this.root, "");
+    }
+    private void display(Node node, String indent) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(indent + node.value);
+        display(node.left, indent + "\t");
+        display(node.right, indent + "\t");
+    }
+
+}
