4ad3cd6eeb0c40cd913c4099448b4a235cde406b
Sandeep Kushwaha <102976365+Sandeep277@users.noreply.github.com>
2024-07-11 00:49:21 +0530

    smallest subArray using sliding window

--- src/algorithms/SlidingWindow.java
+++ src/algorithms/SlidingWindow.java
@@ -2,11 +2,12 @@ package algorithms;
 
 public class SlidingWindow {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 6, 4, 3, 53};
-        System.out.println(maxSum(arr, 2));
+        int[] arr = {1, 2, 6, 4, 3, 5};
+        System.out.println(maxSumSubArray(arr, 2));
+        System.out.println(smallestSubArray(7, arr));
     }
 
-    public static int maxSum(int[] arr, int n) {
+    public static int maxSumSubArray(int[] arr, int n) {
         int currSum = 0;
 
         for (int i = 0; i < n; i++) {
@@ -20,5 +21,22 @@ public class SlidingWindow {
         }
 
         return largestSum;
+    }
+
+    public static int smallestSubArray(int targetSum, int[] arr) {
+        int minWindowSize = Integer.MAX_VALUE;
+        int currentWindowSum = 0;
+        int windowStart = 0;
+        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+            currentWindowSum += arr[windowEnd];
+
+            while(currentWindowSum >= targetSum) {
+                minWindowSize = Math.min(minWindowSize, windowEnd - windowStart + 1);
+                currentWindowSum -= arr[windowStart];
+                windowStart++;
+            }
+        }
+
+        return minWindowSize;
     }
 }
